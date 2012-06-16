eb9b390d8e84a16f7606acc63a1bc4b31ddfd60b
Marek Jelen <marek@jelen.biz>
2012-06-16 19:55:55 +0200

    Remember PID

--- start.sh
+++ start.sh
@@ -13,3 +13,5 @@ ln -s ${OPENSHIFT_REPO_DIR}deployments webapps
 CMD="java -jar start.jar -Djetty.host=$OPENSHIFT_INTERNAL_IP -Djetty.port=$OPENSHIFT_INTERNAL_PORT"
 
 nohup $CMD > $OPENSHIFT_LOG_DIR/server.log 2>&1 &
+
+echo $! > jetty.pid
