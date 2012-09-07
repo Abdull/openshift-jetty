b8807cfbd73409863acebd50ac100655af87f4d3
Abdull <gerrit.huebbers@googlemail.com>
2012-09-07 17:35:30 +0300

    constraining jvm to 384 MiBytes of maximum heap

--- start.sh
+++ start.sh
@@ -10,7 +10,8 @@ fi
 
 ln -s ${OPENSHIFT_REPO_DIR}deployments webapps
 
-CMD="java -jar start.jar -Djetty.host=$OPENSHIFT_INTERNAL_IP -Djetty.port=$OPENSHIFT_INTERNAL_PORT"
+# TODO find optimal Xmx setting... depends on free OpenShift instance and paid OpenShift instance, among other factors 
+CMD="java -Xmx384m -jar start.jar -Djetty.host=$OPENSHIFT_INTERNAL_IP -Djetty.port=$OPENSHIFT_INTERNAL_PORT"
 
 nohup $CMD > $OPENSHIFT_LOG_DIR/server.log 2>&1 &
 
